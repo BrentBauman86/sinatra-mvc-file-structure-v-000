06fa4e1b30d71cf138b400b9095fd8dd59bcb861
Brent Bauman <brentbauman86@gmail.com>
2018-08-14 14:25:58 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,14 @@
-class Model
+class Dog 
 
+    attr_accessor :name, :breed, :age 
+
+    @@all = [] 
+
+    def initialize 
+      @name = name 
+      @breed = breed 
+      @age = age 
+      @@all << self  
+    end
+  end
 end
